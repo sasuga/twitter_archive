322d05f68c9479a922130bf1a17880c1a33f0bd9
hidenobu nagao <hidenobu.nagao@gmail.com>
2022-06-04 09:29:12 +0900

    実装、デバッグ、リファクタ中

--- funcs.py
+++ funcs.py
@@ -1,55 +1,116 @@
 # -*- coding: utf-8 -*-
-#import datetime
 import sys
+import datetime
+from time import sleep
 from requests_oauthlib import OAuth1Session
+API_LIMIT = 429
+API_CORRECT = 200
+
 
 def file_read(f):
-  try:
-      config = open(f,'r')
-  except IndexError:
-      print("config file not found.")
-  except IOError:
-      print("config file not found.")
-  else:
-      return config
+    try:
+        config = open(f, 'r')
+    except IndexError:
+        print("config file not found.")
+    except IOError:
+        print("config file not found.")
+    else:
+        return config
+
 
 def twitter_auth(owner):
     # TODO: 例外処理を組み込む
-    oauth=OAuth1Session(owner["TWI_CK"],
-                        owner["TWI_CS"],
-                        owner["TWI_AT"],
-                        owner["TWI_ATS"])
+    oauth = OAuth1Session(owner["TWI_CK"],
+                          owner["TWI_CS"],
+                          owner["TWI_AT"],
+                          owner["TWI_ATS"])
     return oauth
 
-#def create_list_name(header):
-    #now = datetime.datetime.now()
-    #str_now=now.strftime('%Y%m%d')
-    #return header+str_now
 
 def is_response_ok(response):
-    if response.status_code!=200:
-        print("強制終了：エラーコード："+str(response.status_code))
-        sys.exit()
+    if response.status_code != API_CORRECT:
+        if response.status_code == API_LIMIT:
+            pause_service()
     else:
         return True
 
-def lists_read(owner,app,oauth):
-    params = {  "screen_name":owner["screen_name"],
-                "count":app["list"]["count"]}
+
+def lists_read(owner, app, oauth):
+    params = {"screen_name": owner["screen_name"],
+              "count": app["list"]["count"]}
     # TODO: 例外処理を組み込む
-    return oauth.get(app["end_point"]["find_list"],params=params)
+    return oauth.get(app["end_point"]["find_list"], params=params)
+
 
 # TODO: 要動作確認
-def api_access(ep,type,auth,params):
-    if type=="GET":
-        response = auth.get(ep,params=params)
+def api_access(ep, type, auth, params):
+    if type == "GET":
+        response = auth.get(ep, params=params)
     else:
-        response = auth.post(ep,params=params)
+        response = auth.post(ep, params=params)
 
-    if response.status_code==200:
+    if response.status_code == API_CORRECT:
         return json.loads(respons.text)
     else:
         return False
 
-def archive_friend(app , owners , users_id):
-    return "ok"
+
+def create_list(listname, app, oauth):
+    params = {"name": listname,
+              "mode": app["list"]["mode"],
+              "description": "twitter_archives auto create"
+              }
+    # TODO: 例外処理を組み込む
+    res = oauth.post(app["end_point"]["create_list"], params)
+    if res.status_code != API_CORRECT:
+        if res.status_code == API_LIMIT:
+            pause_service()
+    body = json_loads(res.text)
+    return body["id"]
+
+
+def archive_friend(app, owners, user_id, list_id):
+    # リストに突っ込む
+    params = {"list_id": list_id,
+              "user_id": user_id}
+    # TODO: 例外処理を組み込む
+    res = oauth.post(app["end_point"]["put_friend_list"], params=params)
+    if res.status_code != API_CORRECT:
+        if res.status_code == API_LIMIT:
+            pause_service()
+    # TODO : とりあえず全て成功したとみなそう
+    params = {"user_id": user_id}
+    # TODO: 例外処理を組み込む
+    # TODO : とりあえず全て成功したとみなそう
+    res = oauth.post(app["end_point"]["remove_friend"], params=params)
+    if res.status_code != API_CORRECT:
+        if res.status_code == API_LIMIT:
+            pause_service()
+
+
+def un_archive_friend(app, owners, user_id, list_id):
+    # フォローする
+    params = {"user_id": user_id,
+              "follow": app["friends"]["follow"]}
+    # TODO: 例外処理を組み込む
+    res = oauth.post(app["end_point"]["add_friend"], params=params)
+    if res.status_code != API_CORRECT:
+        if res.status_code == API_LIMIT:
+            pause_service()
+    # TODO : とりあえず全て成功したとみなそう
+
+    # TODO: 例外処理を組み込む
+    # TODO : とりあえず全て成功したとみなそう
+    # リストから外す
+    params = {"user_id": user_id,
+              "list_id": list_id}
+    res = oauth.post(app["end_point"]["remove_friend_list"], params=params)
+    if res.status_code != API_CORRECT:
+        if res.status_code == API_LIMIT:
+            pause_service()
+
+
+def pause_service():
+    print(datetime.datetime.now() + "sleep mode start")
+    sleep(60 * 15)
+    print(datetime.datetime.now() + "sleep mode end")
--- twitter_archive.py
+++ twitter_archive.py
@@ -2,25 +2,18 @@
 #
 # *リストを作成する
 # *twitterアカウントのフォロー一覧を取得する
-# 取得した一覧の最新のツイートのタイムスタンプを取得する
-# 更新が無いアカウントをRリストに登録し、フォローを外す
-# リスト一覧を取得する
-# 命名規則にあうリストがあれば、そのリストの中を検索する
-# もし更新があったアカウントがあれば、フォローしなおして、リストから除外する
-# memo
-#	app.archive.interval
-#	app.list.header
-#	app.list.modeIsPublic
-#       app.list.end_point.create_list
-#       app.list.end_point.xxxxxxxxxx
-##
+# *取得した一覧の最新のツイートのタイムスタンプを取得する
+# *更新が無いアカウントをRリストに登録し、フォローを外す
+# *リスト一覧を取得する
+# *命名規則にあうリストがあれば、そのリストの中を検索する
+# *もし更新があったアカウントがあれば、フォローしなおして、リストから除外する
 
 import json
 import sys
-import pprint # 4DEBUG:
+import pprint  # 4DEBUG:
 import time
-from datetime import datetime,timedelta
-import funcs #original
+from datetime import datetime, timedelta
+import funcs  # original
 
 file = funcs.file_read('config/app.settings.json')
 app = json.load(file)
@@ -28,38 +21,34 @@ file = funcs.file_read('config/owners.settings.json.dev')
 owners = json.load(file)
 accounts = owners["accounts"]
 
+counter = 0
+archive_count = 0
+
 for owner in accounts:
-    #リスト作成処理
-    oauth=funcs.twitter_auth(owner)
+    # リスト作成処理
+    oauth = funcs.twitter_auth(owner)
     listname = app["list"]["name"]
 
-    res = funcs.lists_read(owner,app,oauth)
+    res = funcs.lists_read(owner, app, oauth)
     if funcs.is_response_ok(res):
-        #リスト
-        has_identical_name=False
+        # リスト
+        list_id = 0
         body = json.loads(res.text)
         body = body["lists"]
 
-        for i,listlist in enumerate(body):
-            if listlist["name"]==listname:
-                has_identical_name=True
-        if has_identical_name==False:
-            params = {  "name":listname,
-                        "mode":app["list"]["mode"],
-                        "description":"twitter_archives auto create"
-                        }
-            # TODO: 例外処理を組み込む
-            res = oauth.post(app["end_point"]["create_list"],params)
-            if res.status_code!=200:
-                sys.exit("強制終了：エラーコード："+str(res.status_code))
+        for i, listlist in enumerate(body):
+            if listlist["name"] == listname:
+                list_id = listlist["id"]
+        if list_id==0:
+            list_id = funcs.create_list(listname, app, oauth)
 
-    #フォロー一覧取得処理
+    # フォロー一覧取得処理
     # TODO: 良い感じにリファクタリングしたい
-    params = {  "screen_name":owner["screen_name"],
-                "count":app["friends"]["count"],
-                "stringify_ids":app["friends"]["stringify_ids"]}
+    params = {"screen_name": owner["screen_name"],
+              "count": app["friends"]["count"],
+              "stringify_ids": app["friends"]["stringify_ids"]}
     # TODO: 例外処理を組み込む
-    res = oauth.get(app["end_point"]["get_friend_list"],params=params)
+    res = oauth.get(app["end_point"]["get_friend_list"], params=params)
     if funcs.is_response_ok(res):
         body = json.loads(res.text)
         friends = []
@@ -67,30 +56,48 @@ for owner in accounts:
 
         while body['next_cursor'] != 0:
             params["cursor"] = body["next_cursor"]
-            res = oauth.get(app["end_point"]["get_friend_list"],params=params)
+            res = oauth.get(app["end_point"]["get_friend_list"], params=params)
             # TODO: 例外処理を組み込む
             if funcs.is_response_ok(res):
                 body = json.loads(res.text)
                 friends.extend(body["ids"])
 
-
-    #フォロワーの最新のツイートを取得する
+    # フォロワーの最新のツイートを取得する
     for user_id in friends:
-        params = {  "user_id":user_id,
-                    "count":app["tweet"]["count"],
-                    "exclude_replies":app["tweet"]["exclude_replies"],
-                    "include_rts":app["tweet"]["include_rts"]
-                }
+        # print(user_id)
+        counter += 1
+        params = {"user_id": user_id,
+                  "count": app["tweet"]["count"],
+                  "exclude_replies": app["tweet"]["exclude_replies"],
+                  "include_rts": app["tweet"]["include_rts"]
+                  }
         # TODO: 例外処理を組み込む
-        res = oauth.get(app["end_point"]["get_friend_tweet"],params=params)
+        res = oauth.get(app["end_point"]["get_friend_tweet"], params=params)
 
         if funcs.is_response_ok(res):
             body = json.loads(res.text)
-            struct_time = time.strptime(body[0]["created_at"], '%a %b %d %H:%M:%S +0000 %Y')
-            tw_dt = datetime(*struct_time[:6])
 
-            if datetime.now() > tw_dt + timedelta(days=90):
-                print(funcs.archive_friend(app,owners,user_id))
+            #最新のツイート取得が怪しい。
+            #ほとんど中に入っていかないのではないか。
+            #if 0 in body:
+            struct_time = time.strptime(body[0]["created_at"], '%a %b %d %H:%M:%S +0000 %Y')
+            last_tw_dt = datetime(*struct_time[:6])
+
+            print("最新のツイート、現在の時刻、足しこんだ時刻")
+            print(last_tw_dt)
+            print(datetime.now())
+            print(last_tw_dt + timedelta(days=90))
             sys.exit()
 
+            if datetime.now() > last_tw_dt + timedelta(days=90):
+                archive_count += 1
+                funcs.archive_friend(app, owners, user_id,list_id)
+            #else:
+            archive_count += 1
+            funcs.archive_friend(app, owners, user_id,list_id)
+
+    print("count:" +
+          str(counter) +
+          "  archive:" +
+          str(archive_count))
 #print(json.dumps(data, indent=2))
